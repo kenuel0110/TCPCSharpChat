86b8c91b3347f1e9f7b622a0a8089d70b8ccae3d
kenuel0110 <zeroonezero0110@gmail.com>
2021-10-21 13:27:10 +0300

    Trying add action

--- TCPWPFTest/MainContent.xaml.cs
+++ TCPWPFTest/MainContent.xaml.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,27 +20,116 @@ using TCPCSharpChat;
 
 namespace TCPWPFTest
 {
-    /// <summary>
-    /// Логика взаимодействия для MainContent.xaml
-    /// </summary>
+    
     public partial class MainContent : Page
     {
+
+        private const string host = "127.0.0.1";        //переменная ip адресса
+        private const int port = 55555;                 //переменная порта
+        static TcpClient client;                        //переменная клиента
+        static NetworkStream stream;                    //переменная потока
 
         public string current = "";         //переменая для имени
 
         public MainContent()
         {
+
             InitializeComponent();
-            read4json();
+            read4jsonName();
             label_UserNameMain.Text = current;
+            start();
 
         }
 
-        private void read4json()            //функция чтения json файла
+        public void read4jsonName()            //функция чтения json файла имени
         {
             string json = File.ReadAllText("user.json");
             User users = JsonConvert.DeserializeObject<User>(json);
             current = users.CurrentUser;
         }
+
+        private void start()
+        {
+            string userName = current;
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port); //подключение клиента
+                stream = client.GetStream(); // получаем поток
+
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                // запускаем новый поток для получения данных
+                Thread receiveThread = new Thread(new ThreadStart(receiveMessage));
+                receiveThread.Start(); //старт потока
+                send_notification($"{userName} присоеденился к чату");
+
+            }
+            catch (ObjectDisposedException ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+                disconnect();
+            }
+        }
+
+        private void receiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message);
+                    tb_read_message.AppendText(message);//вывод сообщения
+                }
+                catch
+                {
+                    MessageBox.Show("Подключение прервано!"); //соединение было прервано
+                    disconnect();
+                }
+            }
+        }
+
+        private void disconnect()
+        {
+            if (stream != null)
+                stream.Close();//отключение потока
+            if (client != null)
+                client.Close();//отключение клиента
+            Environment.Exit(0); //завершение процесса
+        }
+
+        private void btn_send_Click(object sender, RoutedEventArgs e)
+        {
+            while (true)
+            {
+                string message = tb_editMessage.Text.ToString();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+
+        public void send_notification(string notification) 
+        {
+            string message = notification;
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+        }
+
     }
 }
